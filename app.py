d6630a59ffb4fbb325093a9cf0eefc5bf6040125
Michael Tran <Mctran1724@gmail.com>
2023-07-11 18:29:20 -0400

    notes and multiple windows

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
 import PySimpleGUI as sg
 from bossing_mule import Bosser
 
+
+## TODO: 
+## Dynamically updating UI elements for each bosser (https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_Layout_Add_and_Delete_Rows.py),
+##  and the calculations and functionality to add boss crystals. Each bosser should bring up a window where you can add those.
+
+
 # GUI Definition
 remaining_crystals = 180
 boss_mules = set()
 
+
+def adding_window():
+    print("Adding Window Opening")
+    layout = [
+        [sg.Text("Character Name: "), sg.Input(key='add_char_name'), sg.Text("Level: "), sg.Input(key='add_level'), sg.Button("Add")]
+    ]
+
+    window = sg.Window("add_window", layout, modal=True)
+    
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED:
+            break
+        elif event == "Add":
+            bosser_name = values['add_char_name']
+            bosser_class = "Class name implemented later"
+            bosser_level = values['add_level']
+            add_remove_bosser(bosser_name, bosser_class, bosser_level, add=True)
+    
+    window.close()
 
 def add_remove_bosser(name: str, job: str, level: int, add: bool) -> None:
     bosser = Bosser(name, job, level)
@@ -26,6 +52,7 @@ def add_remove_bosser(name: str, job: str, level: int, add: bool) -> None:
 
 def display_bossers(sortby = 'level'):
     boss_mule_list = list(boss_mules)
+
     #add functionality to the sorting later
     sortkeys = {
         'level': lambda x: x.level,
@@ -35,44 +62,47 @@ def display_bossers(sortby = 'level'):
 
     boss_mule_list.sort(key=sortkeys[sortby])
 
+    rows = []
+
     #make this a dataframe or table after
-    text = ""
     for bosser in boss_mule_list:
         s = f"Level {bosser.level} {bosser.job} {bosser.name} | {bosser.total_meso} | {bosser.clear_time} \n"
-        text += s 
+        rows.append([s, sg.Button("Remove"), sg.Button("Edit")])
 
-    sg.popup_scrolled(text)
+    return rows
 
-layout = [
-    [sg.Text("Boss Mules Here")],
-    [sg.Text("Character Name: "), sg.Input(key='add_char_name'), sg.Text("Level: "), sg.Input(key='add_level'), sg.Button("Add")],
-    [sg.Text("Character Name: "), sg.Input(key='remove_char_name'), sg.Text("Level: "), sg.Input(key='remove_level'), sg.Button("Remove")],
-    [sg.Button("Calculate"), sg.Button("Display Bossers")]
-]
 
-window = sg.Window("Boss Mule Tracker", layout)
+#main
+def main():
+    main_layout = [
+        [sg.Text("Boss Mules Here")],
+        [sg.Button("Add Bosser"), sg.Button("Update")]
+    ]
 
-while True:
-    event, values = window.read()
+    window = sg.Window("Boss Mule Tracker", main_layout)
 
-    if event == sg.WIN_CLOSED:
-        break
+    while True:
+        event, values = window.read()
 
-    elif event == "Add":
-        bosser_name = values['add_char_name']
-        bosser_class = "Class name implemented later"
-        bosser_level = values['add_level']
-        add_remove_bosser(bosser_name, bosser_class, bosser_level, add=True)
-        
-    elif event == "Remove":
-        bosser_name = values['remove_char_name']
-        bosser_class = "Class name implemented later"
-        bosser_level = values['remove_level']
-        add_remove_bosser(bosser_name, bosser_class, bosser_level, add=False)
+        if event == sg.WIN_CLOSED:
+            break
 
-    elif event == 'Display Bossers':
-        display_bossers()
+        elif event == "Add Bosser":
+            adding_window()
 
-window.close()
+        elif event == 'Update':
+            new_rows = display_bossers()
+            #removing previous rows
+            main_layout = [
+                [sg.Text("Boss Mules Here")],
+                [sg.Button("Add Bosser"), sg.Button("Update")]
+            ]
+            #Now add the new rows back on
+            main_layout += new_rows
 
-print(boss_mules)
+            #Now update the display
+            
+    window.close()
+
+if __name__=="__main__":
+    main()
